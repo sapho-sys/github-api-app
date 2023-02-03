74546d119d9b671a831ed5bb5f20be4b8c1be4c8
Nkunzi <saphonkunzi@gmail.com>
2023-02-03 14:40:42 +0200

    (feat): render github users from api

--- src/components/users/users.jsx
+++ src/components/users/users.jsx
@@ -19,4 +19,64 @@ class Users extends React.Component {
             }
         )
     };
-}
+
+    renderStat(stat) {
+        return (
+            <li key={stat.name} className='user-info_stat'>
+                <Link to={stat.url}>
+                    <p className='user-info_stat-value'>{stat.value}</p>
+                    <p className='user-info_stat-name'>{stat.name}</p>
+                </Link>
+            </li>
+        )
+    }
+
+    render() {
+        if(!this.state.user) {
+            return (
+                <div className='user-page'>LOADING DATA...</div>
+            )
+        }
+        const user = this.state.user;
+
+        const stats = [
+
+            {
+                name: 'Public Repos',
+                value: user.public_repos,
+                url: `/user/${this.props.params.username}/repos`
+            },
+            {
+                name: 'Followers',
+                value: user.followers,
+                url: `/user/${this.props.params.username}/followers`
+            },
+            {
+                name: 'Following',
+                value: user.following,
+                url: `/user/${this.props.params.username}/following`
+            }
+
+        ];
+
+        return (
+            <div className='user-page'>
+                <div className='user-info'>
+                    <Link className='user-info_text' to={`/user/${user.login}`}>
+                        <img className='user-info_avatar' src={user.avatar_url} alt={`${user.login} avatar`}/>
+                        <h2 className="user-info_title">{user.login} ({user.name})</h2>
+                        <p className='user-info_bio'>{user.bio}</p>
+                    </Link>
+
+                    <ul className='user-info_stats'>
+                        {stats.map(this.renderStat)}
+                    </ul>
+                </div>
+            </div>
+        )
+
+
+    }
+};
+
+export default Users
