d3b93f53442194c2d1027b9e54db3fa8c7bd8274
Nkunzi <saphonkunzi@gmail.com>
2023-02-06 16:28:59 +0200

    (feat): Fetching data in react complete

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -1,28 +1,87 @@
 import React from 'react';
-import { browserHistory as history } from 'react-router';
+// import { browserHistory as history } from 'react-router';
 
 
-class Search extends React.Component {
-   constructor(props){
-    super(props);
-    this._handleEntry = this._handleEntry.bind(this);
-}
-_handleEntry(e){
-    e.preventDefault();
-    history.push(`/user/${this.ref.userInput.value}`);
-}
+// class Search extends React.Component {
+//    constructor(props){
+//     super(props);
+//     this._handleEntry = this._handleEntry.bind(this);
+// }
+// _handleEntry(e){
+//     e.preventDefault();
+//     history.push(`/user/${this.ref.userInput.value}`);
+// }
  
- render(){
-    return(
-        <div class='search-page'>
-            <h2>Search for your favoured Rockstar Developer:</h2>
-            <form onSubmit={this._handleEntry}>
-                <input ref="userInput" className='search-page_input' type='text' />
-                <button className='search-page_button'>Search</button>
-            </form>
-        </div>
-    );
- }
-};
+//  render(){
+//     return(
+//         <div class='search-page'>
+//             <h2>Search for your favoured Rockstar Developer:</h2>
+//             <form onSubmit={this._handleEntry}>
+//                 <input ref="userInput" className='search-page_input' type='text' />
+//                 <button className='search-page_button'>Search</button>
+//             </form>
+//         </div>
+//     );
+//  }
+// };
 
-export default Search;
+// export default Search;
+
+
+//new component
+{/* <div>
+<h1>My Hacker Stories</h1>
+
+<InputWithLabel
+  id="search"
+  value={searchTerm}
+  isFocused
+  onInputChange={handleSearch}
+>
+  <strong>Search:</strong>
+</InputWithLabel>
+
+<hr />
+
+{stories.isError && <p>Something went wrong ...</p>}
+
+{stories.isLoading ? (
+  <p>Loading ...</p>
+) : (
+  <List
+    list={searchedStories}
+    onRemoveItem={handleRemoveStory}
+  />
+)}
+</div> */}
+
+// const InputWithLabel = ({
+//     id,
+//     value,
+//     type = 'text',
+//     onInputChange,
+//     isFocused,
+//     children,
+//   }) => {
+//     const inputRef = React.useRef();
+  
+//     React.useEffect(() => {
+//       if (isFocused) {
+//         inputRef.current.focus();
+//       }
+//     }, [isFocused]);
+  
+//     return (
+//       <>
+//         <label htmlFor={id}>{children}</label>
+//         &nbsp;
+//         <input
+//           ref={inputRef}
+//           id={id}
+//           type={type}
+//           value={value}
+//           onChange={onInputChange}
+//         />
+//       </>
+//     );
+//   };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
-import Search from "./components/search";
-import Users from "./components/users";
-import { Router } from "express";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-const routes = (
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Search} />
-      <Route path="/user:/username" component={Users} />
-    </Route>
-  </Router>
-);
-ReactDOM.render(routes, document.getElementById("root"));
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
