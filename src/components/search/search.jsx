db9c375cc8755c7bc5fced831a8e8a18b861c0a5
Nkunzi <saphonkunzi@gmail.com>
2023-02-06 16:53:51 +0200

    (feat): re-fetch api data complete

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -12,24 +12,9 @@ import React from 'react';
 //     history.push(`/user/${this.ref.userInput.value}`);
 // }
  
-//  render(){
-//     return(
-//         <div class='search-page'>
-//             <h2>Search for your favoured Rockstar Developer:</h2>
-//             <form onSubmit={this._handleEntry}>
-//                 <input ref="userInput" className='search-page_input' type='text' />
-//                 <button className='search-page_button'>Search</button>
-//             </form>
-//         </div>
-//     );
-//  }
-// };
-
-// export default Search;
-
-
-//new component
-{/* <div>
+ render(){
+    return(
+        <div>
 <h1>My Hacker Stories</h1>
 
 <InputWithLabel
@@ -53,7 +38,40 @@ import React from 'react';
     onRemoveItem={handleRemoveStory}
   />
 )}
-</div> */}
+</div> 
+    );
+ }
+};
+
+// export default Search;
+
+
+//new component
+{ <div>
+<h1>My Hacker Stories</h1>
+
+<InputWithLabel
+  id="search"
+  value={searchTerm}
+  isFocused
+  onInputChange={handleSearch}
+>
+  <strong>Search:</strong>
+</InputWithLabel>
+
+<hr />
+
+{stories.isError && <p>Something went wrong ...</p>}
+
+{stories.isLoading ? (
+  <p>Loading ...</p>
+) : (
+  <List
+    list={searchedStories}
+    onRemoveItem={handleRemoveStory}
+  />
+)}
+</div> }
 
 // const InputWithLabel = ({
 //     id,
